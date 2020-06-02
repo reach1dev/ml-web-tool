11d14b30413c98303960254f773b5ced0344bc2f
reach1dev <reach1dev@hotmail.com>
2020-06-02 21:21:44 +0100

    add mla

--- src/actions/Constants.js
+++ src/actions/Constants.js
@@ -1,2 +1,2 @@
-//export const BaseUrl = 'https://api-ml-web-tool.herokuapp.com'
-export const BaseUrl = 'http://localhost:5000'
+export const BaseUrl = 'https://api-ml-web-tool.herokuapp.com'
+//export const BaseUrl = 'http://localhost:5000'
