882b4b7ded8b9b694a61368ad5ab23a31aa05577
reach1dev <reach1dev@hotmail.com>
2020-06-03 08:01:55 +0100

    remove parameters for kmean algorithm for now

--- src/components/TransformParameters.js
+++ src/components/TransformParameters.js
@@ -7,23 +7,25 @@ export const TransformParameters = {
       name: 'n_clusters',
       type: 'number',
       default: 8
-    }, {
-      name: 'n_init',
-      type: 'number',
-      default: 10
-    }, {
-      name: 'tol',
-      type: 'number',
-      default: 0.0001
-    }, {
-      name: 'precompute_distances',
-      type: 'boolean',
-      default: 'auto'
-    }, {
-      name: 'algorithm',
-      type: 'string',
-      default: 'auto'
-    }]
+    }
+    // , {
+    //   name: 'n_init',
+    //   type: 'number',
+    //   default: 10
+    // }, {
+    //   name: 'tol',
+    //   type: 'number',
+    //   default: 0.0001
+    // }, {
+    //   name: 'precompute_distances',
+    //   type: 'boolean',
+    //   default: 'auto'
+    // }, {
+    //   name: 'algorithm',
+    //   type: 'string',
+    //   default: 'auto'
+    // }
+  ]
   }, {
     type: 1,
     parameters: [{
